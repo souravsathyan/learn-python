29a9488776a3972539f299d99c1c712d99efb21e
souravsathyan <souravksathyan@gmail.com>
2025-02-16 01:29:51 +0530

    13 on going

---
+++ 12 string slicing/code.py
@@ -0,0 +1,7 @@
+fruit = 'Mango'
+print(len(fruit))
+print(fruit[0:4])  # fruit[0:4] [including this : not including this]
+print(fruit[0:-3])  # means fruit[0:len(fruit)-3]
+
+nm = 'Harry'
+print(nm[-4:-2])
